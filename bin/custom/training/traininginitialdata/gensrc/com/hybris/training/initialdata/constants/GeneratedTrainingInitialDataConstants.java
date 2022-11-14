9468a1b5eb37ce7bdb290c77267ad3b3f17001de
Yazid Yasykur <yasykur@gmail.com>
2022-11-14 21:19:18 +0700

    Submit tugas hybris 1

---
+++ bin/custom/training/traininginitialdata/gensrc/com/hybris/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 14, 2022, 11:04:17 AM                   ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
